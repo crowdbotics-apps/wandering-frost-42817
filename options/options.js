372c045e5e9fb24eb4627b932bbbdc18f8f84394
Crowdbotics <team@crowdbotics.com>
2023-08-13 11:48:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-frost-42817",
+  url: "https://wandering-frost-42817.botics.co",
+  api: "https://wandering-frost-42817.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
